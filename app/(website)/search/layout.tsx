eaf549ede69b971c9b577893a89f5f85f2954b67
lokigen921 <lokigen921@gmail.com>
2025-05-02 18:00:29 +0300

    feat: Add separate layout for search page

---
+++ app/(website)/search/layout.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// A simple layout for the search section that doesn't include Sanity preview logic
+export default function SearchLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>; 
+} 
